171ba2af734912cdd6ebe7d8ffd963c5fc5acffe
Finn <finnterdal@gmail.com>
2020-06-26 09:04:37 -0500

    morning review

--- demo.js
+++ demo.js
@@ -1,2 +1,33 @@
-const Sequelize = require("sequelize")
-const db = new Sequelize("postgres://localhost/sandwich-mart")
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+const db = new Sequelize('postgres://localhost/sandwich-mart');
+
+const Sandwich = db.define('sandwich', {
+  name: Sequelize.STRING,
+});
+
+async function seedDb() {
+  await db.sync({ force: true });
+
+  await Sandwich.create({ name: 'Roast Beef' });
+  await Sandwich.create({ name: 'BLT' });
+  await Sandwich.create({ name: 'Reuben' });
+  await Sandwich.create({ name: 'Grilled Cheese' });
+
+  // Use a Symbol Operator
+  const [ roastBeef, reuben ] = await Sandwich.findAll({
+    where: {
+      name: {
+        [Op.startsWith]: 'R'
+      }
+    }
+  });
+
+  console.log(roastBeef.name)
+  console.log(reuben.name)
+
+  // console.log(rSandwiches.map(sand => sand.name))
+
+  await db.close();
+}
+seedDb();
